ac2a16ae54ebf57bab9bdf7406df4696953d601a
Manish Kumar <manish84398@gmail.com>
2023-12-22 04:47:16 +0530

    v-0.1-prettier

--- public/dom.js
+++ public/dom.js
@@ -1,45 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const chatBox = document.getElementById('chat-box');
-    const messageInput = document.getElementById('promptForm');
-    
-    messageInput.addEventListener('submit', async function(event) {
-      event.preventDefault();
-  
-      const prompt = document.getElementById('prompt').value;
-  
-      appendMessage('You', prompt, 'user');
-  
-      const response = await fetch('/generate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({ prompt }),
-      });
-  
-      const generatedText = await response.text();
-      appendMessage('AI', generatedText, 'ai');
-      
-      document.getElementById('prompt').value = '';
-    });
-  
-    function appendMessage(sender, text, type) {
-      const messageDiv = document.createElement('div');
-      messageDiv.className = `message ${type}`;
-      messageDiv.innerHTML = `<strong>${sender}:</strong> ${text}`;
-      chatBox.appendChild(messageDiv);
-      chatBox.scrollTop = chatBox.scrollHeight;
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const chatBox = document.getElementById("chat-box");
+  const messageInput = document.getElementById("promptForm");
 
-    async function fetchChatHistory() {
-      const response = await fetch('/chat-history');
-      const history = await response.json();
-  
-      history.forEach(({ sender, text, type }) => {
-        appendMessage(sender, text, type);
-      });
-    }
-  
-    fetchChatHistory();
+  messageInput.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const prompt = document.getElementById("prompt").value;
+
+    appendMessage("You", prompt, "user");
+
+    const response = await fetch("/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({ prompt }),
+    });
+
+    const generatedText = await response.text();
+    appendMessage("AI", generatedText, "ai");
+
+    document.getElementById("prompt").value = "";
   });
-  
+
+  function appendMessage(sender, text, type) {
+    const messageDiv = document.createElement("div");
+    messageDiv.className = `message ${type}`;
+    messageDiv.innerHTML = `<strong>${sender}:</strong> ${text}`;
+    chatBox.appendChild(messageDiv);
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+
+  async function fetchChatHistory() {
+    const response = await fetch("/chat-history");
+    const history = await response.json();
+
+    history.forEach(({ sender, text, type }) => {
+      appendMessage(sender, text, type);
+    });
+  }
+
+  fetchChatHistory();
+});
